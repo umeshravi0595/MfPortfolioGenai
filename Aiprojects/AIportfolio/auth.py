868a40c450f085a71d95f4f84a5166fee584ea92
umeshravi0595 <107918224+umeshravi0595@users.noreply.github.com>
2026-05-03 08:18:21 +0530

    commited auth file

---
+++ Aiprojects/AIportfolio/auth.py
@@ -0,0 +1,31 @@
+from kiteconnect import KiteConnect
+import os
+import hashlib
+from dotenv import load_dotenv
+
+load_dotenv()
+
+KITE_API_KEY = os.getenv("KITE_API_KEY")
+KITE_API_SECRET = os.getenv("KITE_API_SECRET")
+
+
+def get_login_url():
+    """
+    Returns Zerodha login URL
+    """
+    kite = KiteConnect(api_key=KITE_API_KEY)
+    return kite.login_url()
+
+
+def generate_session(request_token):
+    """
+    Exchanges request_token for access_token
+    """
+    kite = KiteConnect(api_key=KITE_API_KEY)
+
+    data = kite.generate_session(
+        request_token,
+        api_secret=KITE_API_SECRET
+    )
+
+    return data["access_token"]
